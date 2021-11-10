9ab9eac4df589ac53e4432bd30fd7c85a0a1188e
Shadow Monarch <walid1mhidou@gmail.com>
2021-11-10 17:16:20 +0000

    new commit

--- Huffman.java
+++ Huffman.java
@@ -16,6 +16,21 @@ class MyComparator implements Comparator<HuffmanNode> {
     }
 }
 class public Huffman{
+
+public static void printCode(HuffmanNode root, String s)
+        {
+
+
+        if (root.left == null && root.right == null && Character.isLetter(root.c)) {
+        System.out.println(root.c + ":" + s);
+        return;
+        }
+
+
+        printCode(root.left, s + "0");
+        printCode(root.right, s + "1");
+        }
+
     public static void main(String[]args){
 
     }
