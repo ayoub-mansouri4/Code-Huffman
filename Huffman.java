8a34f59d670675e55d208364d0b891ac5dceb3ba
Shadow Monarch <walid1mhidou@gmail.com>
2021-11-10 17:02:05 +0000

    second commit

--- Huffman.java
+++ Huffman.java
@@ -1,1 +1,10 @@
+// node class is the basic structure
+// of each node present in the Huffman - tree.
+class HuffmanNode {
 
+    int data;
+    char c;
+
+    HuffmanNode left;
+    HuffmanNode right;
+}
